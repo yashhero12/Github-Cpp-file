dcbad55d957a5a80a6dfd56d87b672a23930fc27
yashhero <yashhero19799@gmail.com>
2025-07-20 20:57:57 +0530

    c++ learning

--- practice/main.cpp
+++ practice/main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <string>
 
 int main(){
-    auto result = (10 <=> 20) > 0 ;
-    std::cout << result << std::endl;
+    // int age;
+    // std::string name;
+    // std::cout << "please enter your name and age : " << std::endl;
+    
+    // // std::cin >> name;
+    // // std::cin >> age;
+    
+    // std::cin >> name >> age;
+
+    // std::cout << "Hello " << name << " You are " << age << "year old " << std::endl;
+
+    // data with spaces
+    // std::string fullname;
+    // int age2;
+    // std::cout << "Sir enter your Full name : ";
+    // std::getline(std::cin,fullname);
+    // std::cout << "Sir enter your age : ";
+    // std::cin>>age2;
+    // std::cout << "Hello " << fullname << " You are " << age2 << "year old " << std::endl;
+
+    // int random ; 
+    // std::cout<<"random value : "<<random<<std::endl;
+
+    
+
+    return 0;
 }
