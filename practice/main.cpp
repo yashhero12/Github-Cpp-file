020ebcdb317dcb25241b8748cce3b54f78280d18
yashhero <yashhero19799@gmail.com>
2025-08-06 21:09:55 +0530

    day making own spotify

--- practice/main.cpp
+++ practice/main.cpp
@@ -1,45 +1,17 @@
 #include <iostream>
 #include <string>
 
-int stringmodulesuseing(){
-    std::string variable;
-    // to store a string we use string module
-    std::cout<<"enter your variable or string you wana to store : ";
-    std::getline(std::cin,variable);
-    std::cout<<"here is your variable : "<<variable<<std::endl;
-    return 0;
-}
 
 
 int main(){
 
-    stringmodulesuseing();
+    int nameofarray[5] {1,2,3,4,5};
 
+    std::cout<<"to print value we use loops"<<std::endl;
 
-    // int age;
-    // std::string name;
-    // std::cout << "please enter your name and age : " << std::endl;
-    
-    // // std::cin >> name;
-    // // std::cin >> age;
-    
-    // std::cin >> name >> age;
-
-    // std::cout << "Hello " << name << " You are " << age << "year old " << std::endl;
-
-    // data with spaces
-    // std::string fullname;
-    // int age2;
-    // std::cout << "Sir enter your Full name : ";
-    // std::getline(std::cin,fullname);
-    // std::cout << "Sir enter your age : ";
-    // std::cin>>age2;
-    // std::cout << "Hello " << fullname << " You are " << age2 << "year old " << std::endl;
-
-    // int random ; 
-    // std::cout<<"random value : "<<random<<std::endl;
-
-
+    for(int i:nameofarray){
+        std::cout<<"value : "<<i<<std::endl;
+    }
 
     return 0;
 }
